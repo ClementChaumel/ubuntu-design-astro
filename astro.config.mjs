2b1b8e5351222b66535cd98a4796c0c3a9fd5572
ClementChaumel <clement.chaumel@gmail.com>
2023-07-19 17:04:02 +0200

    Add gh pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://clementchaumel.github.io",
+  base: "/ubuntu-design-astro",
   integrations: [
     prefetch({
       selector: "a[href^='/']",
